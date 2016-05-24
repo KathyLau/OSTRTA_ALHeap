d9ff3315a201d582e7c1e7c98187995d1a74a501
Calvin Vuong <cvuong@stuy.edu>
2016-05-24 18:33:57 -0400

    comment added

--- HeapSort.java
+++ HeapSort.java
@@ -6,9 +6,12 @@ HW44 -- Sort of Like Magic
 */
 import java.util.ArrayList;
 public class HeapSort{
+    
+    // swaps elements in positions 1 and 2, in ArrayList a
     private static void swap( ArrayList a, int pos1, int pos2 ) {
 	a.set( pos1, a.set( pos2, a.get(pos1) ) );
     }
+
     public static ArrayList<Integer> Heapify(ArrayList<Integer> a){
 	for (int count = 0 ; count<a.size(); count++){
 	    if (count*2+1 > a.size()-1){
