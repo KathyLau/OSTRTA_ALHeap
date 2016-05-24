5c98b4804afddfa670bb08e2fee9f7e5b0030213
Kathy Lau <slau7@stuy.edu>
2016-05-24 18:39:13 -0400

    heapify

--- HeapSort.java
+++ HeapSort.java
@@ -6,33 +6,25 @@ HW44 -- Sort of Like Magic
 */
 import java.util.ArrayList;
 public class HeapSort{
-    
+
     // swaps elements in positions 1 and 2, in ArrayList a
     private static void swap( ArrayList a, int pos1, int pos2 ) {
 	a.set( pos1, a.set( pos2, a.get(pos1) ) );
     }
 
-    public static ArrayList<Integer> Heapify(ArrayList<Integer> a){
-	for (int count = 0 ; count<a.size(); count++){
-	    if (count*2+1 > a.size()-1){
-	    }
-	    else if (count*2+2 > a.size()-1){
-		if (a.get(count*2+1)<a.get(count)){
-		    swap(a, count, count*2+1);
-		    count=-1;
-		}
-	    }
-	    else if (a.get(count*2+1)<a.get(count*2+2) && a.get(count*2+1) < a.get(count)){
-		swap(a, count, count*2+1);
-		count=-1;
-	    }
-	    else if (a.get(count*2+2)<a.get(count*2+1) && a.get(count*2+2) < a.get(count)){
-		swap(a, count, count*2+2);
-		count=-1;
-	    }
-	}
-	return a;
-    }
+    private static void heapify( ArrayList<Integer> a ) {
+	for( int i=1; i< a.size(); i++ ) {
+	    int valPos = i;
+	    while( valPos > 0 ) {
+        int parentPos = (valPos-1) / 2;
+    		if ( a.get(valPos) < a.get(parentPos) ) {
+    		    swap( a, valPos, parentPos );
+    		    valPos = parentPos;}
+    		else break;
+    	    }
+        }
+      }
+
     public static void main(String[] args){
 	ArrayList<Integer> test = new ArrayList();
 	test.add(3);
@@ -41,7 +33,8 @@ public class HeapSort{
 	test.add(4);
 	test.add(2);
 	test.add(9);
-	System.out.println(Heapify(test));
+	heapify(test);
+  System.out.println(test);
     }
-	
+
 }
