7ca4b1bd785e1437bf30a22fb51d33cb528b8ca7
wuruochong <rwu3@stuy.edu>
2016-05-24 18:29:17 -0400

    Heapify done

--- HeapSort.java
+++ HeapSort.java
@@ -4,6 +4,41 @@ APCS2 PD10
 HW44 -- Sort of Like Magic
 2016-05-24
 */
-
+import java.util.ArrayList;
 public class HeapSort{
+    private static void swap( ArrayList a, int pos1, int pos2 ) {
+	a.set( pos1, a.set( pos2, a.get(pos1) ) );
+    }
+    public static ArrayList<Integer> Heapify(ArrayList<Integer> a){
+	for (int count = 0 ; count<a.size(); count++){
+	    if (count*2+1 > a.size()-1){
+	    }
+	    else if (count*2+2 > a.size()-1){
+		if (a.get(count*2+1)<a.get(count)){
+		    swap(a, count, count*2+1);
+		    count=-1;
+		}
+	    }
+	    else if (a.get(count*2+1)<a.get(count*2+2) && a.get(count*2+1) < a.get(count)){
+		swap(a, count, count*2+1);
+		count=-1;
+	    }
+	    else if (a.get(count*2+2)<a.get(count*2+1) && a.get(count*2+2) < a.get(count)){
+		swap(a, count, count*2+2);
+		count=-1;
+	    }
+	}
+	return a;
+    }
+    public static void main(String[] args){
+	ArrayList<Integer> test = new ArrayList();
+	test.add(3);
+	test.add(6);
+	test.add(7);
+	test.add(4);
+	test.add(2);
+	test.add(9);
+	System.out.println(Heapify(test));
+    }
+	
 }
